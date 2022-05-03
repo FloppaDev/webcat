eb37060c62b797726db50cc61901fadb52d5831d
Floppa <rschuhler@hotmail.fr>
2022-05-03 13:07:16 +0200

    init

---
+++ gen/data.js
@@ -0,0 +1,19 @@
+
+const DATA = {
+
+    shaders: [
+        //{{shaders}}
+        //e.g.  shader_name: { vert: "...", frag: "..." }
+    ],
+
+    textures: [
+        //{{images}}
+        //e.g.  texture_name: "path"
+    ],
+
+    sounds: [
+        //{{sounds}}
+        //e.g.  sound_name: "path"
+    ]
+
+};
---
+++ src/prelude.js
@@ -0,0 +1,10 @@
+
+// Everything in the prelude is accessible from all modules
+// without any import needed.
+
+const log = console.log;
+const err = console.err;
+
+// Whole module imports will use this syntax:
+// import * as $module_name from "module_path";
+
