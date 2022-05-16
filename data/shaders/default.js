5681b1f622e9aad5cf62cf3d61ec530ad3d81b06
Floppa <rschuhler@hotmail.fr>
2022-05-16 03:56:07 +0200

    Shader class

--- data/shaders/default.js
+++ data/shaders/default.js
@@ -2,33 +2,43 @@
 export class Pipeline {
 
     // Bind vertex buffer.
-    vbo = (engine /*engine.js:Engine*/, ctx /*engine.renderer.ctx*/) => {
-        ctx.bindBuffer(ctx.ARRAY_BUFFER, engine.renderer.quad_vbo);
-    };
+    vbo(renderer /*renderer.js:Renderer*/) {
+        let {ctx, quad_vbo} = renderer;
+        ctx.bindBuffer(ctx.ARRAY_BUFFER, quad_vbo);
+    }
 
     // Record VAO attributes for the vertex shader.
-    vao = (
-        engine /*engine.js:Engine*/,
-        ctx /*engine.renderer.ctx*/, 
-        program /*engine.renderer.shader_programs[x]*/
-    ) => {
+    vao(
+        renderer /*renderer.js:Renderer*/, 
+        program /*renderer.js:Renderer.shader_programs[x]*/
+    ) {
+        let {ctx} = renderer;
+
         let pos = ctx.getAttribLocation(program, "pos");
         ctx.bindAttribLocation(program, pos, "pos");
 
         ctx.vertexAttribPointer(pos, 2, ctx.FLOAT, ctx.FALSE, 0, 0);
         ctx.enableVertexAttribArray(0);
-    };
+    }
 
     // Bind UBOs for draw.
-    ubos = (
-        engine /*engine.js:Engine*/,
-        ctx /*engine.renderer.ctx*/, 
+    ubos(
+        renderer /*renderer.js:Renderer*/, 
         program /*engine.renderer.shader_programs[x]*/,
         material /*engine.materials[x]*/
-    ) => {
-        engine.bind_camera(ctx, program);
+    ) {
+        let {ctx} = renderer;
+
+        renderer.bind_camera(ctx, program);
         ctx.bindTexture(ctx.TEXTURE_2D, material.texture);
-    };
+    }
+
+    draw(
+        renderer /*renderer.js:Renderer*/,
+        shader_module /*Object.entries(data.js:Data.shaders)[x]*/
+    ) {
+        //TODO
+    }
 
 }
 
--- src/pipelines.js
+++ src/pipelines.js
@@ -1,4 +1,34 @@
 
-const build = () => {
-    //TODO
-};
+export class Shader {
+
+    constructor(
+        vert /*e.g. default.vert*/,
+        frag /*e.g. default.frag*/,
+        module /*e.g. data/shaders/default.js*/
+    ) {
+        this.vert = vert;
+        this.frag = frag;
+        this.module = module;
+        this.pipeline = new module.Pipeline();
+        this.material = new module.Material();
+    }
+
+    build(renderer /*renderer.js:Renderer*/) {
+        //TODO
+    };
+
+    bind(renderer /*renderer.js:Renderer*/) {
+        let {ctx} = renderer;
+
+        ctx.useProgram(/*TODO*/);
+
+        //TODO bind uniforms.
+
+        //TODO bind VAO.
+    }
+
+    draw(renderer /*renderer.js:Renderer*/) {
+        this.pipeline.draw(renderer);
+    }
+
+}
