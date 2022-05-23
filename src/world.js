aa52af01d08fae2708a4aa0a43252c886bf65d9f
Floppa <rschuhler@hotmail.fr>
2022-05-23 21:08:34 +0200

    World layout

--- src/world.js
+++ src/world.js
@@ -1,9 +1,60 @@
 
-export class World {
+//TODO
+
+export class VertexBuffer {
 
     constructor() {
-        this.draw_calls = [];
         //TODO
     }
 
 }
+
+export class IndexBuffer {
+
+    constructor() {
+        //TODO
+    }
+
+}
+
+export class Primitive {
+
+    constructor(
+        vertex_buffer /*VertexBuffer*/, 
+        index_buffer /*IndexBuffer*/, 
+        material /*shaders:Shader.material*/
+    ){
+        this.vertex_buffer = vertex_buffer;
+        this.index_buffer = index_buffer;
+        this.material = material;
+    }
+
+}
+
+export class DrawCall {
+
+    constructor(primitive /*Primitive*/, transforms /*TODO*/) {
+        this.object = object;
+        this.transforms = transforms;
+    }
+
+}
+
+export class Dispatch {
+
+    constructor(shader /*shaders.js:Shader*/, draw_call /*DrawCall*/) {
+        this.shader = shader;
+        this.draw_call = draw_call;
+    }
+
+}
+
+export class World {
+
+    constructor() {
+        this.draw_calls = [];
+        this.primitives = [];
+    }
+
+}
+
