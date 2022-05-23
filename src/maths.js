a98d1b2064df11eb6664cda8c574770e666b270c
Floppa <rschuhler@hotmail.fr>
2022-05-23 21:14:38 +0200

    Transform layout

--- src/maths.js
+++ src/maths.js
@@ -7,3 +7,20 @@ export class V2 {
     }
 
 }
+
+export class Transform {
+
+    constructor(
+        position /*V2*/, 
+        depth /*float*/,
+        angle /*float*/, 
+        scale /*V2*/
+    ) {
+        this.position = position;
+        this.angle = angle;
+        this.scale = scale;
+
+        //TODO matrix
+    }
+
+}
