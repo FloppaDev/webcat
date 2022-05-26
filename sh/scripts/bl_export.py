a5ca8d55900cc39297d17b4ca7a37a4da5cd4bfa
Floppa <rschuhler@hotmail.fr>
2022-05-26 09:59:04 +0200

    vertices

--- sh/scripts/bl_export.py
+++ sh/scripts/bl_export.py
@@ -1,6 +1,11 @@
 
 import bpy
 
+class Vertex:
+    def __init__(self, position, uv):
+        self.position = position
+        self.uv = uv
+
 class Primitive:
     def __init__(self): 
         self.indices = []
@@ -10,34 +15,49 @@ class Mesh:
         self.vertices = []
         self.primitives = []
 
+class Object:
+    def __init__(self):
+        self.mesh = Mesh()
+
 for ob in bpy.data.objects:
-    print (ob.name)
-    try:
-        ob.material_slot_remove()
-        print ("removed material from " + ob.name)
-    except:
-        print (ob.name + " does not have materials.")
+    print('\n'+ob.name)
 
-    data = ob.data
+    if ob.type == 'MESH':
+        mesh = ob.data
 
-    try:
-        data.validate_material_indices()
-        data.calc_loop_triangles()
+        out_object = Object()
 
-        materials = data.materials
-        vertices = data.vertices
+        mesh.validate_material_indices()
+        mesh.calc_loop_triangles()
 
-        loop_triangles = data.loop_triangles
-        loops = data.loops
+        materials = mesh.materials
+        
+        for i in range(0, len(materials)):
+            out_object.mesh.primitives.append(Primitive())
+
+        vertices = mesh.vertices
+        uv_map = mesh.uv_layers[0].data
+
+        out_object.mesh.vertices = [None] * len(uv_map)
+
+        loop_triangles = mesh.loop_triangles
+        loops = mesh.loops
 
         for loop_triangle in loop_triangles:
             material_index = loop_triangle.material_index
+            out_primitive = out_object.mesh.primitives[material_index]
 
             for loop in loop_triangle.loops:
                 vertex_loop = loops[loop]
+
                 index = vertex_loop.vertex_index
                 vertex = vertices[index] 
                 position = vertex.co
+                uv = uv_map[index].uv
 
-    except:
-        print(ob.name + " does not have mesh data.")
+                out_primitive.indices.append(index)
+                out_object.mesh.vertices[index] = Vertex(position, uv)
+
+    else:
+        pass
+        #TODO get object 
